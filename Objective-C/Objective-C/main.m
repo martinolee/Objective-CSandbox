0d982eb8d5e184d5e91c61fd2a74e636460394f0
martinolee <soohan.m.lee@gmail.com>
2020-07-02 23:03:08 +0900

    Add command line tool

---
+++ Objective-C/Objective-C/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Objective-C
+//
+//  Created by Soohan Lee on 2020/07/02.
+//  Copyright © 2020 Soohan Lee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      NSLog(@"Hello, World!");
+  }
+  return 0;
+}
