8a14eaef51b4aa466f1a7b67892653fa8f63fae1
martinolee <soohan.m.lee@gmail.com>
2020-07-05 23:10:04 +0900

    Create person class

---
+++ Objective-C/Objective-C/Person.h
@@ -0,0 +1,17 @@
+//
+//  Person.h
+//  Objective-C
+//
+//  Created by Soohan Lee on 2020/07/05.
+//  Copyright © 2020 Soohan Lee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Person : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Objective-C/Objective-C/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  Objective-C
+//
+//  Created by Soohan Lee on 2020/07/05.
+//  Copyright © 2020 Soohan Lee. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
